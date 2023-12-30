5b5fa1257d4dea1ac324b2bdbac378fb2d0f41df
taham8875 <taha.m8875@gmail.com>
2023-12-31 01:50:28 +0200

    markdown editor

---
+++ project 2 - Markdown Editor/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
+
+const inputContent = ref('# hello')
+const outputContent = computed(() => DOMPurify.sanitize(marked(inputContent.value) as string))
+</script>
+<template>
+  <div class="flex h-screen">
+    <textarea class="bg-gray-100 w-[50%] p-5 font-mono focus:outline-0 resize-none min-h-screen" v-model="inputContent" />
+    <div class="p-5 w-6/12" v-html="outputContent"></div>
+  </div>
+</template>
+<style></style>
